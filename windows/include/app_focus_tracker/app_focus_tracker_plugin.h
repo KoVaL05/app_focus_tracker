3ec4022a5ce42baed3df84582983d0bc7980d17e
KoVaL05 <lukasz.kowalski1105@gmail.com>
2025-07-15 13:44:29 +0200

    fix

---
+++ windows/include/app_focus_tracker/app_focus_tracker_plugin.h
@@ -0,0 +1,10 @@
+// This header is provided for external consumers (e.g., generated_plugin_registrant.cc)
+// It simply forwards to the main plugin implementation header located in the
+// Windows source directory. Keeping all implementation details in one place
+// reduces duplication while satisfying include path expectations.
+#ifndef FLUTTER_PLUGIN_APP_FOCUS_TRACKER_PLUGIN_H_
+#define FLUTTER_PLUGIN_APP_FOCUS_TRACKER_PLUGIN_H_
+
+#include "../../app_focus_tracker_plugin.h"
+
+#endif // FLUTTER_PLUGIN_APP_FOCUS_TRACKER_PLUGIN_H_ 
